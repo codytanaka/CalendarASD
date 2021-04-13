4db59a37d5c854a782ceffdc0af9110cab868ad2
Agung Yuwono Sugiyono <agungabadi9999@gmail.com>
2021-04-13 20:03:15 +0700

    Button di header udh bisa

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,9 @@ const calendar = document.getElementById('calendar');
 function load(){
     // load the current day for references
     const dt = new Date();
+    if(nav != 0){
+        dt.setMonth(new Date().getMonth() + nav);
+    }
     // get the date month and year from dt
     const date = dt.getDate();
     const month = dt.getMonth();
@@ -26,6 +29,8 @@ function load(){
     // get how many day in this month
     // do this by constructing a new Date onject and passing the value for the next month and the value zero to get the last day in this month
     const daysInMonth = new Date(year, month+1, 0).getDate();
+    const maxDateInPrevMonth = new Date(year, month, 0).getDate();
+    console.log(maxDateInPrevMonth);
     // get the first date in this month
     const firstDayinMonth = new Date(year, month, 1)
     
@@ -38,20 +43,39 @@ function load(){
     const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
     let displayMonth = dt.toLocaleDateString('en-us', {month: 'long'});
     document.getElementById('month-display').innerText = `${displayMonth} ${year}`;
+    // Clear the calendar div from the square
+    calendar.innerHTML = '';
 
-    for(let i=1; i<=paddingDays + daysInMonth; ++i){
+    for(let i=1; i<=42; ++i){
         const daySquare = document.createElement('div');
         daySquare.classList.add('day');
 
-        if(i>paddingDays){
+        if(i>paddingDays && i<=paddingDays + daysInMonth){
             daySquare.innerHTML = i-paddingDays;
             daySquare.addEventListener('click', ()=> console.log('click on', i-paddingDays));
         }else {
             daySquare.classList.add('padding');
+            if(i<=paddingDays){
+                daySquare.innerHTML = maxDateInPrevMonth - (paddingDays - i);
+            }else if(i>paddingDays + daysInMonth){
+                daySquare.innerHTML = i-(paddingDays + daysInMonth);
+            }
         }
 
         calendar.appendChild(daySquare);
     }
 }
 
+function initButton(){
+    document.getElementById('next-button').addEventListener('click', ()=> {
+        nav++;
+        load();
+    });
+    document.getElementById('back-button').addEventListener('click', ()=> {
+        nav--;
+        load();
+    });
+}
+
+initButton();
 load();
