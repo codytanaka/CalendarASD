a0035c2f597489fbbf7127edca3d28c735f0854d
codytanaka <cody.tanaka@binus.ac.id>
2021-04-14 21:52:45 +0700

    insert

--- assets/script.js
+++ assets/script.js
@@ -9,19 +9,67 @@ if(localStorage.getItem('events')){
 }else {
     event = [];
 }
+
 // initialize event and then load event from localStorage. If there is no event set event to an empty array
 
 // initialize some constant variable
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
+
+function openModal(date) {
+  clicked = date;
+
+  const eventForDay = event.find(e => e.date === clicked);
+
+  if (eventForDay) {
+    document.getElementById('eventText').innerText = eventForDay.title;
+    deleteEventModal.style.display = 'block';
+  } else {
+    newEventModal.style.display = 'block';
+  }
+
+  backDrop.style.display = 'block';
+}
+
+function closeModal() {
+  eventTitleInput.classList.remove('error');
+  newEventModal.style.display = 'none';
+  deleteEventModal.style.display = 'none';
+  backDrop.style.display = 'none';
+  eventTitleInput.value = '';
+  clicked = null;
+  load();
+}
+
+function saveEvent() {
+  if (eventTitleInput.value) {
+    eventTitleInput.classList.remove('error');
+
+    event.push({
+      date: clicked,
+      title: eventTitleInput.value, 
+    });
+
+    localStorage.setItem('event', JSON.stringify(event));
+    closeModal();
+  } else {
+    eventTitleInput.classList.add('error');
+  }
+}
 
 // initialize function load
 function load(){
     // load the current day for references
     const dt = new Date();
-    if(nav != 0){
-        dt.setMonth(new Date().getMonth() + nav);
+
+    if (nav !== 0) {
+    dt.setMonth(new Date().getMonth() + nav);
     }
+
     // get the date month and year from dt
     const date = dt.getDate();
     const month = dt.getMonth();
@@ -29,8 +77,6 @@ function load(){
     // get how many day in this month
     // do this by constructing a new Date onject and passing the value for the next month and the value zero to get the last day in this month
     const daysInMonth = new Date(year, month+1, 0).getDate();
-    const maxDateInPrevMonth = new Date(year, month, 0).getDate();
-    console.log(maxDateInPrevMonth);
     // get the first date in this month
     const firstDayinMonth = new Date(year, month, 1)
     
@@ -43,39 +89,39 @@ function load(){
     const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
     let displayMonth = dt.toLocaleDateString('en-us', {month: 'long'});
     document.getElementById('month-display').innerText = `${displayMonth} ${year}`;
-    // Clear the calendar div from the square
-    calendar.innerHTML = '';
 
-    for(let i=1; i<=42; ++i){
+    calendar.innerHTML='';
+    for(let i=1; i<=paddingDays + daysInMonth; ++i){
         const daySquare = document.createElement('div');
         daySquare.classList.add('day');
 
-        if(i>paddingDays && i<=paddingDays + daysInMonth){
+        if(i>paddingDays){
             daySquare.innerHTML = i-paddingDays;
-            daySquare.addEventListener('click', ()=> console.log('click on', i-paddingDays));
+            daySquare.addEventListener('click', ()=>openModal(`${month + 1}/${i - paddingDays}/${year}`) );
         }else {
             daySquare.classList.add('padding');
-            if(i<=paddingDays){
-                daySquare.innerHTML = maxDateInPrevMonth - (paddingDays - i);
-            }else if(i>paddingDays + daysInMonth){
-                daySquare.innerHTML = i-(paddingDays + daysInMonth);
-            }
         }
 
         calendar.appendChild(daySquare);
     }
 }
 
-function initButton(){
-    document.getElementById('next-button').addEventListener('click', ()=> {
-        nav++;
-        load();
-    });
-    document.getElementById('back-button').addEventListener('click', ()=> {
-        nav--;
-        load();
-    });
+function initButtons() {
+  document.getElementById('next-button').addEventListener('click', () => {
+    nav++;
+    load();
+  });
+
+  document.getElementById('back-button').addEventListener('click', () => {
+    nav--;
+    load();
+  });
+
+  document.getElementById('saveButton').addEventListener('click', saveEvent);
+  document.getElementById('cancelButton').addEventListener('click', closeModal);
+  // document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+  // document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
-initButton();
+initButtons();
 load();
